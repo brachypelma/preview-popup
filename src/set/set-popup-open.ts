3a1cf20ae99c5548b487a6330214c5729a110957
Eric Jonas <ericdjonas@gmail.com>
2023-07-12 06:13:10 -0400

    Added comment

--- src/set/set-popup-open.ts
+++ src/set/set-popup-open.ts
@@ -5,7 +5,9 @@ export default function setPopupOpen(
   const iframe = popup.querySelector('.preview-popup__iframe')
 
   if (!iframe) return
-  
+
+  // Position popup based on mouse position/document boundaries
+
   iframe.setAttribute('src', href)
   popup.showModal()
 }
